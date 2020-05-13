78536c61f19326df229630b524e53c0e5600c0d5
pavelp21 <pavelvp2182@yandex.ru>
2020-05-13 18:28:56 +0500

    Main page is typeseted

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,118 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
 
-$(document).ready(function() {
+$(document).ready(function () {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this),
+            button = $('.button-form', this),
+            switch_title = $('.switch-title'),
+            switch_text = $('.switch-text'),
+            currDate = new Date(),
+            startDate = new Array(
+                new Date("May 05 2020 18:30 UTC+3"),
+                new Date("May 06 2020 18:30 UTC+3"),
+                new Date("May 07 2020 18:30 UTC+3")),
+            endDate = new Array(
+                new Date("May 05 2020 21:00 UTC+3"),
+                new Date("May 06 2020 21:00 UTC+3"),
+                new Date("May 07 2020 21:00 UTC+3")),
+            rooms = new Array(
+                'https://pruffme.com/landing/u8937/tmp1588147140',
+                'https://pruffme.com/landing/u8937/tmp1588147140',
+                'https://pruffme.com/landing/u8937/tmp1588147140');
 
+        function roomSwitch() {
+            switch_title.html('А Вы записались на БЕСПЛАТНЫЙ ВЕБИНАР?<br>ЖДЕМ ТОЛЬКО ВАС!')
+            switch_text.removeClass('hide').html('<span class="hurryup">Скорее ЗАХОДИТЕ прямо СЕЙЧАС на вебинар!</span>');
+            button.text('Зайти!');
+        }
+        if ((currDate > startDate[0] && currDate < endDate[0]) || (currDate > startDate[1] && currDate < endDate[1]) || (currDate > startDate[2] && currDate < endDate[2])) {
+            roomSwitch();
+        }
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+                if (currDate > startDate[0] && currDate < endDate[0]) {
+                    window.open(rooms[0]);
+                }
+                if (currDate > startDate[1] && currDate < endDate[1]) {
+                    window.open(rooms[1]);
+                }
+                if (currDate > startDate[2] && currDate < endDate[2]) {
+                    window.open(rooms[2]);
+                }
 
-
-/*Конец документа*/
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            message.slideUp(500);
+        });
+    });
+    /*Модалки*/
+    $('.button-up').fancybox();
+    $(".video-wrapper-revs img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1"  allowfullscreen></iframe>')
+    });
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            buttons: [
+                'slideShow',
+                'share',
+                'zoom',
+                'fullScreen',
+                // 'download',
+                'close'
+            ],
+            speed: 330,
+            loop: true,
+            opacity: "auto",
+            // autoScale: true,
+            mouseWheel: true,
+            transitionEffect: 'slide'
+        });
+    });
+    $(function () {
+        $('#video-revs').on('slide.bs.carousel', function () {
+            $('.video-wrapper-revs iframe').each(function () {
+                var l = $(this).parent().attr('data-img');
+                $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+            });
+            $(".video-wrapper-revs img").click(function () {
+                var a = $(this).parent().attr("data-youtube");
+                $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>');
+            });
+        });
+    });
+    /*Конец документа*/
 });
